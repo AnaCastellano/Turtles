c806b105b34a66fb9cbd83e35d9f44274bc1dee5
Ana Castellano <39617111+AnaCastellano@users.noreply.github.com>
2019-07-17 08:11:17 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TurtlesGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGameSettingsFile = new System.Windows.Forms.Button();
+            this.btnMovesFile = new System.Windows.Forms.Button();
+            this.lblGameSettingsFile = new System.Windows.Forms.Label();
+            this.lblMoveFile = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.pnlGame = new System.Windows.Forms.Panel();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnGameSettingsFile
+            // 
+            this.btnGameSettingsFile.Location = new System.Drawing.Point(12, 12);
+            this.btnGameSettingsFile.Name = "btnGameSettingsFile";
+            this.btnGameSettingsFile.Size = new System.Drawing.Size(220, 23);
+            this.btnGameSettingsFile.TabIndex = 0;
+            this.btnGameSettingsFile.Text = "Add Game Settings file";
+            this.btnGameSettingsFile.UseVisualStyleBackColor = true;
+            this.btnGameSettingsFile.Click += new System.EventHandler(this.BtnGameSettingsFile_Click);
+            // 
+            // btnMovesFile
+            // 
+            this.btnMovesFile.Location = new System.Drawing.Point(12, 41);
+            this.btnMovesFile.Name = "btnMovesFile";
+            this.btnMovesFile.Size = new System.Drawing.Size(220, 23);
+            this.btnMovesFile.TabIndex = 1;
+            this.btnMovesFile.Text = "Add Moves file";
+            this.btnMovesFile.UseVisualStyleBackColor = true;
+            this.btnMovesFile.Click += new System.EventHandler(this.BtnMovesFile_Click);
+            // 
+            // lblGameSettingsFile
+            // 
+            this.lblGameSettingsFile.AutoSize = true;
+            this.lblGameSettingsFile.Location = new System.Drawing.Point(253, 17);
+            this.lblGameSettingsFile.Name = "lblGameSettingsFile";
+            this.lblGameSettingsFile.Size = new System.Drawing.Size(0, 13);
+            this.lblGameSettingsFile.TabIndex = 3;
+            // 
+            // lblMoveFile
+            // 
+            this.lblMoveFile.AutoSize = true;
+            this.lblMoveFile.Location = new System.Drawing.Point(253, 58);
+            this.lblMoveFile.Name = "lblMoveFile";
+            this.lblMoveFile.Size = new System.Drawing.Size(0, 13);
+            this.lblMoveFile.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 70);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(220, 23);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // pnlGame
+            // 
+            this.pnlGame.Location = new System.Drawing.Point(12, 99);
+            this.pnlGame.Name = "pnlGame";
+            this.pnlGame.Size = new System.Drawing.Size(531, 457);
+            this.pnlGame.TabIndex = 6;
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.Location = new System.Drawing.Point(562, 102);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(400, 446);
+            this.lbResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 568);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.pnlGame);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblMoveFile);
+            this.Controls.Add(this.lblGameSettingsFile);
+            this.Controls.Add(this.btnMovesFile);
+            this.Controls.Add(this.btnGameSettingsFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Turtles Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGameSettingsFile;
+        private System.Windows.Forms.Button btnMovesFile;
+        private System.Windows.Forms.Label lblGameSettingsFile;
+        private System.Windows.Forms.Label lblMoveFile;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Panel pnlGame;
+        private System.Windows.Forms.ListBox lbResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace TurtlesGame
+{
+    public partial class Form1 : Form
+    {
+        static string pathGameSettingsFile { get; set; }
+        static string pathMovesFile { get; set; }
+        static int boardX { get; set; }
+        static int boardY { get; set; }
+        static char startDir { get; set; }
+        static int startX { get; set; }
+        static int startY { get; set; }
+        static int exitX { get; set; }
+        static int exitY { get; set; }
+        static int[,] mines { get; set; }
+        static char[] moves { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGameSettingsFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog gameSettingsFile = new OpenFileDialog();
+                gameSettingsFile.InitialDirectory = Directory.GetCurrentDirectory();
+                gameSettingsFile.Title = "Browse Json Game settings file";
+                gameSettingsFile.DefaultExt = "json";
+                gameSettingsFile.CheckFileExists = true;
+                gameSettingsFile.CheckPathExists = true;
+                gameSettingsFile.ShowDialog();
+                pathGameSettingsFile = gameSettingsFile.FileName;
+                lblGameSettingsFile.Text = pathGameSettingsFile;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        private void BtnMovesFile_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog gameSettingsFile = new OpenFileDialog();
+                gameSettingsFile.InitialDirectory = Directory.GetCurrentDirectory();
+                gameSettingsFile.Title = "Browse Json Moves file";
+                gameSettingsFile.DefaultExt = "json";
+                gameSettingsFile.CheckFileExists = true;
+                gameSettingsFile.CheckPathExists = true;
+                gameSettingsFile.ShowDialog();
+                pathMovesFile = gameSettingsFile.FileName;
+                lblMoveFile.Text = pathGameSettingsFile;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbResult.Items.Clear();
+                if (isValid())
+                {
+                    SetInitValuesGame();
+
+                    TableLayoutPanel tbBoard = new TableLayoutPanel();
+                    CreateBoard(ref tbBoard);
+
+                    //Load the initial turtle
+                    LoadTurtleIcon(ref tbBoard,startDir,startX,startY );
+
+                    //Load Exit
+                    LoadExitIcon(ref tbBoard);
+
+                    //Load mines
+                    LoadMines(ref tbBoard);
+
+                    //Execute moves
+                    ExecuteMoves(ref tbBoard);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void SetInitValuesGame()
+        {
+            try
+            {
+                StreamReader jsonStream = File.OpenText(pathGameSettingsFile);
+                var json = jsonStream.ReadToEnd();
+                gameSettings gameSettingsAux = JsonConvert.DeserializeObject<gameSettings>(json);
+
+                jsonStream = File.OpenText(pathMovesFile);
+                json = jsonStream.ReadToEnd();
+                moves movesAux = JsonConvert.DeserializeObject<moves>(json);
+
+                boardX = gameSettingsAux.board[0];
+                boardY = gameSettingsAux.board[1];
+                startX = gameSettingsAux.startPos[0];
+                startY = gameSettingsAux.startPos[1];
+                startDir = gameSettingsAux.startDir;
+                exitX = gameSettingsAux.exitPos[0];
+                exitY = gameSettingsAux.exitPos[1];
+                mines = gameSettingsAux.mines;
+                moves = movesAux.m;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void CreateBoard(ref TableLayoutPanel tbBoard)
+        {
+            try
+            {
+                tbBoard.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                tbBoard.ColumnCount = boardX;
+                tbBoard.RowCount = boardY;
+                int sizeAux = (int)100 / boardX;
+                for (int i = 0; i < boardX; i++)
+                {
+                    tbBoard.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, sizeAux));
+                    tbBoard.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                }
+                sizeAux = (int)100 / boardY;
+                for (int i = 0; i < boardY; i++)
+                {
+                    RowStyle auxRow = new RowStyle(SizeType.Percent, sizeAux);
+                    tbBoard.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+                    tbBoard.RowStyles.Add(auxRow);
+                }
+                tbBoard.Dock = DockStyle.Fill;
+                tbBoard.AutoSize = false;
+                pnlGame.Controls.Add(tbBoard);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void LoadTurtleIcon(ref TableLayoutPanel tbBoard, char dir, int posX, int posY)
+        {
+            PictureBox pbTurtle = new PictureBox();
+            int rotateAngle = 0;
+            switch (dir)
+            {
+                case 'N':
+                    rotateAngle = 0;
+                    break;
+                case 'E':
+                    rotateAngle = 90;
+                    break;
+                case 'S':
+                    rotateAngle = 180;
+                    break;
+                case 'W':
+                    rotateAngle = 270;
+                    break;
+            }
+            pbTurtle.Image = RotateImage(Properties.Resources.turtle, rotateAngle);
+            tbBoard.Controls.Add(pbTurtle, startX, startY);
+        }
+
+        public void LoadExitIcon (ref TableLayoutPanel tbBoard)
+        {
+            PictureBox pbExit = new PictureBox();
+            pbExit.Image = Properties.Resources.exit;
+            tbBoard.Controls.Add(pbExit, exitX, exitY);
+        }
+
+        public void LoadMines (ref TableLayoutPanel tbBoard)
+        {
+            int mineX = 0;
+            int mineY = 0;
+            PictureBox pbMine;
+            for (int m = 0; m < mines.GetLength(0); m++)
+            {
+                mineX = mines[m, 0];
+                mineY = mines[m, 1];
+                pbMine = new PictureBox();
+                pbMine.Image = Properties.Resources.mine;
+                tbBoard.Controls.Add(pbMine, mineX, mineY);
+            }
+        }
+
+        public void ExecuteMoves(ref TableLayoutPanel tbBoard)
+        {
+            char dir = startDir;
+            int x = startX;
+            int y = startY;
+            string aux = "";
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'r')
+                {
+                    switch (dir)
+                    {
+                        case 'N':
+                            dir = 'E';
+                            break;
+                        case 'E':
+                            dir = 'S';
+                            break;
+                        case 'S':
+                            dir = 'W';
+                            break;
+                        case 'W':
+                            dir = 'N';
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (dir)
+                    {
+                        case 'N':
+                            y -= 1;
+                            break;
+                        case 'E':
+                            x += 1;
+                            break;
+                        case 'S':
+                            y += 1;
+                            break;
+                        case 'W':
+                            x -= 1;
+                            break;
+                    }
+                }
+                aux = moves[i].ToString() + "'- x: " + x.ToString() + " y: " + y.ToString() + " - ";
+                if (x < 0 || y < 0 || x > boardX || y > boardY)
+                { 
+                    lbResult.Items.Add("Squence '" + aux + (i + 1).ToString() + ": Failure!");
+                }
+                else if ((x == exitX) && (y == exitY))
+                {
+                    lbResult.Items.Add("Squence '" + aux + (i + 1).ToString() + ": Exit");
+                }
+                else
+                {
+                    bool mineHit = false;
+                    int[,] minesAux = mines;
+                    int rows = minesAux.GetLength(0);
+                    int cols = minesAux.GetLength(1);
+                    for (int j = 0; j < rows; j++)
+                    {
+                        if ((minesAux[j, 0] == x) && (minesAux[j, 1] == y))
+                        {
+                            mineHit = true;
+                        }
+                    }
+                    if (mineHit == true)
+                    {
+                        lbResult.Items.Add("Sequence '" + aux + (i + 1).ToString() + " : Mine hit!");
+                    }
+                    else
+                    {
+                        lbResult.Items.Add("Sequence '" + aux + (i + 1).ToString() + " : Success!");
+                    }
+                }
+            }
+        }
+        public static Image RotateImage(Image img, float rotationAngle)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+            Graphics gfx = Graphics.FromImage(bmp);
+
+            gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
+            gfx.RotateTransform(rotationAngle);
+            gfx.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
+            gfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            gfx.DrawImage(img, new Point(0, 0));
+            gfx.Dispose();
+
+            return bmp;
+        }
+
+        private bool isValid()
+        {
+            bool result = false;
+            if ((lblGameSettingsFile.Text != "") && (lblMoveFile.Text != ""))
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+    public partial class gameSettings
+    {
+        public int[] board { get; set; }
+        public int[] startPos { get; set; }
+        public char startDir { get; set; }
+        public int[] exitPos { get; set; }
+        public int[,] mines { get; set; }
+    }
+    public partial class moves
+    {
+        public Char[] m { get; set; }
+    }
+
+}
+
+
